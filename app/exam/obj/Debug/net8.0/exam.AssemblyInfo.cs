7c480978b783dacd36284da9fadc6a4c93c46949
Мартынюк Георгий <georgijmartynuk@gmail.com>
2025-12-25 22:17:45 +0300

    Изменения файлов проекта

--- app/exam/obj/Debug/net8.0/exam.AssemblyInfo.cs
+++ app/exam/obj/Debug/net8.0/exam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe958f79d8bc99213d8813cf145741221506039f")]
 [assembly: System.Reflection.AssemblyProductAttribute("exam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
